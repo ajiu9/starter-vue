d6f8ec8ca59d9c933da04b60f114eed2f77aea87
longbiao_wu <615944323@qq.com>
2025-04-11 12:23:31 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,5 @@
+# pkg-placeholder
+
+## License
+
+[MIT](./LICENSE) License © 2024-PRESENT [Ajiu9](https://github.com/ajiu9)
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://ajiu9.cn/" target="_blank">
+      <img src="./assets/logo.svg" class="logo vue" alt="ajiu9 logo" />
+    </a>
+  </div>
+  <HelloWorld msg="你好" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
